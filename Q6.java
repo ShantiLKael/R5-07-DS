7d0b2a2436a800374bc817ca046ce400ad8e085a
ShantiLKael <98842719+ShantiLKael@users.noreply.github.com>
2024-11-20 14:37:19 +0100

    Create Q6.java

---
+++ Q6.java
@@ -0,0 +1,21 @@
+public class Q6
+{
+    public static void main(String[] args) // Bonne correction à vous
+    {
+        if (args.length == 0)
+        {
+            System.out.println("Pour éxecuter ce programme : java Etoile <nbEtoiles>");
+            return;
+        }
+        
+        int nbEtoiles = Integer.parseInt(args[0]);
+        String sRet = "";
+        for (int i = nbEtoiles; i > 0; i--)
+        {
+            sRet += "X".repeat(i);
+            sRet += "\n";
+        }
+        
+        System.out.println(sRet);
+    }
+}
