6b4cea3c197e3b239633fbf0e916fa04127800ae
ShantiLKael <98842719+ShantiLKael@users.noreply.github.com>
2024-11-20 14:24:14 +0100

    Create Q5.java

---
+++ Q5.java
@@ -0,0 +1,21 @@
+public class Q5
+{
+    public static void main(String[] args) // Bonne correction à vous
+    {
+        if (args.length == 0)
+        {
+            System.out.println("Pour éxecuter ce programme : java Etoile <nbEtoiles>");
+            return;
+        }
+        
+        int nbEtoiles = Integer.parseInt(args[0]);
+        String sRet = "";
+        for (int i = nbEtoiles; i > 0; i--)
+        {
+            sRet += "*".repeat(i);
+            sRet += "\n";
+        }
+        
+        System.out.println(sRet);
+    }
+}
